5a60ea1dc0a1326b3b3ff5640ce0ac295ebfd6d7
leellun <leellun@sina.cn>
2022-08-15 17:03:34 +0800

    BTreeNode<T> node

--- src/com/newland/algorithm/tree/redtree/RBTree.java
+++ src/com/newland/algorithm/tree/redtree/RBTree.java
@@ -3,13 +3,48 @@ package com.newland.algorithm.tree.redtree;
 import java.util.Deque;
 import java.util.LinkedList;
 
+/**
+ * 红黑树这个是本人自己写的，按照https://pansong291.gitee.io/web/html/other/RedBlackTree.html 这个跳动进行的
+ * 添加操作打破红黑树的触发条件：父节点是红
+ * 添加操作需要旋转的判定：
+ * （1）uncle节点是null
+ * （2）uncle节点是black
+ *
+ * @param <T>
+ */
 public class RBTree<T extends Comparable> {
     private RBTreeNode<T> root;
 
-    public void addNode(T value) {
+    public T addNode(T value) {
         RBTreeNode<T> node = new RBTreeNode<>(value);
         node.makeRed();
-        addNode(node);
+        return addNode(node);
+    }
+
+    public void removeNode(T value) {
+        if (root == null) return;
+        RBTreeNode<T> currentNode = root;
+        while (currentNode != null) {
+            int cmp = currentNode.value.compareTo(value);
+            if (cmp < 0) {
+                currentNode = currentNode.left;
+            } else if (cmp > 0) {
+                currentNode = currentNode.right;
+            } else {
+                RBTreeNode<T> minNode = findMinData(currentNode);
+                currentNode.setValue(minNode.value);
+                if (minNode.left != null) {
+                    minNode.left.setParent(minNode.parent);
+                    if (minNode.parent != null) {//放置minNode为root的null异常
+                        minNode.parent.setLeft(minNode.left);
+                    }
+                    fixRemove(minNode.left);
+                }
+                //移除minnode
+                minNode.setLeft(null);
+                minNode.setParent(null);
+            }
+        }
     }
 
     private T addNode(RBTreeNode<T> node) {
@@ -32,9 +67,64 @@ public class RBTree<T extends Comparable> {
         return node.value;
     }
 
+    /**
+     * 删除操作平衡
+     *
+     * @param node
+     */
+    private void fixRemove(RBTreeNode<T> node) {
+        while (true) {
+            if (node == null || node == root) {
+                return;
+            }
+            RBTreeNode<T> parent = node.parent;
+            if (parent == null) {
+                node.makeBlack();
+                return;
+            } else if (node.isRed()) {
+                node.makeBlack();
+                return;
+            }
+            RBTreeNode<T> left = parent.left;
+            RBTreeNode<T> right = parent.right;
+            if (left == node) {
+                if (right != null && right.isRed()) {
+                    right.makeBlack();
+                    parent.makeRed();
+                    rotateLeft(parent);
+                }
+                if (right == null) {
+                    node = parent;
+                }else{
+                }
+            } else {
+
+            }
+
+        }
+    }
+//    void balanceDeletion(BTreeNode<T> node){
+//        for(){
+//            //if当前节点是否是更节点
+//            //else if 当前节点的父节点是否是空节点
+//            //else if 当前节点是否是红色
+//            //else if 当前节点是左子树
+//            //if 当前节点的兄弟节点红色
+//            //if 当前节点兄弟节点为空 循环父节点
+//            //else
+//            //if 兄弟节点的 子节点是black
+//            //else
+//            // if 兄弟节点的右节点black 如果兄弟节点左节点存在black
+//            //   兄弟节点red 右旋转  兄弟节点的右节点=
+//
+//        }
+
+
+//    }
+
     private void fixInsert(RBTreeNode<T> node) {
         RBTreeNode<T> parent = node.parent;
-        while (parent != null && parent.red) {
+        while (parent != null && parent.isRed()) {
             RBTreeNode<T> uncle = getUncle(node);
             RBTreeNode<T> ancestor = parent.parent;
             if (ancestor == null) {
@@ -151,6 +241,24 @@ public class RBTree<T extends Comparable> {
             }
         }
         return root;
+    }
+
+    /**
+     * 找到node 找到最大的点
+     * 该红黑树算法，删除一个node，则
+     *
+     * @param node
+     * @return
+     */
+    private RBTreeNode<T> findMinData(RBTreeNode<T> node) {
+        RBTreeNode<T> findNode = node;
+        RBTreeNode<T> minNode = node.left;
+
+        while (minNode != null) {
+            findNode = minNode;
+            minNode = minNode.right;
+        }
+        return findNode;
     }
 
     public void preOrderByStack() {
