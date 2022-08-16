ad978879173a1bee53b4027f2faeb2cbe4b9c92c
leellun <leellun@sina.cn>
2022-08-16 15:14:54 +0800

    ssibingRight

--- src/com/newland/algorithm/tree/redtree/RBTree.java
+++ src/com/newland/algorithm/tree/redtree/RBTree.java
@@ -1,5 +1,6 @@
 package com.newland.algorithm.tree.redtree;
 
+import javax.swing.tree.TreeNode;
 import java.util.Deque;
 import java.util.LinkedList;
 
@@ -33,12 +34,23 @@ public class RBTree<T extends Comparable> {
             } else {
                 RBTreeNode<T> minNode = findMinData(currentNode);
                 currentNode.setValue(minNode.value);
+
                 if (minNode.left != null) {
                     minNode.left.setParent(minNode.parent);
                     if (minNode.parent != null) {//放置minNode为root的null异常
                         minNode.parent.setLeft(minNode.left);
                     }
-                    fixRemove(minNode.left);
+                    if (minNode.isBlack()) {
+                        this.root = fixRemove(this.root, minNode.left);
+                    }
+                } else {
+                    minNode.right.setParent(minNode.parent);
+                    if (minNode.parent != null) {//放置minNode为root的null异常
+                        minNode.parent.setLeft(minNode.right);
+                    }
+                    if (minNode.isBlack()) {
+                        this.root = fixRemove(this.root, minNode.right);
+                    }
                 }
                 //移除minnode
                 minNode.setLeft(null);
@@ -62,7 +74,7 @@ public class RBTree<T extends Comparable> {
                 p.setLeft(node);
             }
             node.setParent(p);
-            fixInsert(node);
+            this.root = fixInsert(this.root, node);
         }
         return node.value;
     }
@@ -72,30 +84,85 @@ public class RBTree<T extends Comparable> {
      *
      * @param node
      */
-    private void fixRemove(RBTreeNode<T> node) {
+    private RBTreeNode<T> fixRemove(RBTreeNode<T> root, RBTreeNode<T> node) {
         while (true) {
-            if (node == null || node == root) {
-                return;
+            if (node == null || node == root) {//当前node定位到root
+                return root;
             }
             RBTreeNode<T> parent = node.parent;
-            if (parent == null) {
+            if (parent == null) {//定位node父节点为null，则为root节点
                 node.makeBlack();
-                return;
+                return node;
             } else if (node.isRed()) {
+                //定位节点为红色，重置定位节点为黑色，因为进行删除的节点在定位节点或子树
+                //经过多次旋转，黑色节点的层级会变成之前的 n-1，所以设置定位节点为黑色
                 node.makeBlack();
-                return;
+                return root;
             }
             RBTreeNode<T> left = parent.left;
             RBTreeNode<T> right = parent.right;
+            //如果定位节点左子树
             if (left == node) {
+                //如果右节点为红色，则父节点为黑色，那么设置父节点红色、右节点黑色，然后左旋转平衡定位节点
                 if (right != null && right.isRed()) {
                     right.makeBlack();
                     parent.makeRed();
-                    rotateLeft(parent);
+                    //左旋转
+                    root = rotateLeft(root, parent);
+                    //左旋转后 parent的右节点变为之前右节点的左节点，需要重新赋值
+                    right = parent.right;
                 }
+                //如果右节点为空，定位节点上移一层
                 if (right == null) {
                     node = parent;
-                }else{
+                } else {
+                    //兄弟节点的左节点和右节点
+                    RBTreeNode<T> siblingLeft = right.left, sibingRight = right.right;
+                    //如果兄弟节点的左右节点都为黑色则定位节点重置为当前定位节点的上一层，即parent
+                    //黑 嘿嘿  、红嘿嘿
+                    if (isBlack(siblingLeft) && isBlack(sibingRight)) {
+                        right.makeRed();
+                        node = parent;
+                    } else {
+                        //如果兄弟节点的左节点是红色右节点是黑色
+                        //需要将左节点设置
+                        if (isBlack(sibingRight)) {
+                            //这里是右节点的左节点是否为空
+                            //注意：这里是jdk的源码逻辑，个人认为，这里不可能为空
+                            if (siblingLeft != null) {
+                                //由红变黑
+                                siblingLeft.makeBlack();
+                            }
+                            // 右节点设置为红色
+                            right.makeRed();
+                            //因为siblingLeft设置了黑色，所以需要右旋转平衡right子树
+                            root = rotateRight(root, right);
+                            //重新获取right
+                            right = parent.right;
+                            //
+                            siblingLeft = right.left;
+                            sibingRight = right.right;
+                        }
+                        //right：黑  sl：黑 sr:红
+                        //right：黑  sl：红 sr：黑=》right：黑 sl：未知 sr：红
+                        // 通过分析 sibingRight在这里不为空一定是红
+                        if (right != null) {
+                            if (parent == null) {
+                                right.makeBlack();
+                            } else {
+                                right.red = parent.red;
+                            }
+                            //将右子树设置为黑色
+                            if (sibingRight != null) {
+                                sibingRight.makeBlack();
+                            }
+                        }
+                        if (parent != null) {//以为需要删除一个定位点为黑色的节点所以需要左旋转平衡节点
+                            parent.makeBlack();
+                            root = rotateLeft(root, parent);
+                        }
+                        node = root;
+                    }
                 }
             } else {
 
@@ -112,17 +179,15 @@ public class RBTree<T extends Comparable> {
 //            //if 当前节点的兄弟节点红色
 //            //if 当前节点兄弟节点为空 循环父节点
 //            //else
-//            //if 兄弟节点的 子节点是black
+//            //if 兄弟节点的 子节点全是black
 //            //else
 //            // if 兄弟节点的右节点black 如果兄弟节点左节点存在black
 //            //   兄弟节点red 右旋转  兄弟节点的右节点=
 //
 //        }
-
-
 //    }
 
-    private void fixInsert(RBTreeNode<T> node) {
+    private RBTreeNode<T> fixInsert(RBTreeNode<T> root, RBTreeNode<T> node) {
         RBTreeNode<T> parent = node.parent;
         while (parent != null && parent.isRed()) {
             RBTreeNode<T> uncle = getUncle(node);
@@ -135,9 +200,9 @@ public class RBTree<T extends Comparable> {
                     //非直线的三点
                     boolean isRight = node == parent.right;
                     if (isRight) {
-                        rotateLeft(parent);
+                        root = rotateLeft(root, parent);
                     }
-                    rotateRight(ancestor);
+                    root = rotateRight(root, ancestor);
                     if (isRight) {
                         node.makeBlack();
                     } else {
@@ -146,9 +211,9 @@ public class RBTree<T extends Comparable> {
                 } else {
                     boolean isLeft = node == parent.left;
                     if (isLeft) {
-                        rotateRight(parent);
+                        root = rotateRight(root, parent);
                     }
-                    rotateLeft(ancestor);
+                    root = rotateLeft(root, ancestor);
                     if (isLeft) {
                         node.makeBlack();
                     } else {
@@ -169,8 +234,7 @@ public class RBTree<T extends Comparable> {
                 parent = node.parent;
             }
         }
-        root.makeBlack();
-        root.parent = null;
+        return root;
     }
 
     private RBTreeNode<T> getUncle(RBTreeNode<T> node) {
@@ -187,35 +251,39 @@ public class RBTree<T extends Comparable> {
     /**
      * 左旋转   左下
      */
-    private void rotateLeft(RBTreeNode<T> node) {
-        RBTreeNode<T> child = node.right;
+    private RBTreeNode<T> rotateLeft(RBTreeNode<T> root, RBTreeNode<T> node) {
+        RBTreeNode<T> right = node.right;
         RBTreeNode<T> parent = node.parent;
         if (parent == null) {
-            this.root = child;
+            root = right;
+            root.makeBlack();
         } else {
-            parent.right = child;
+            parent.right = right;
         }
-        node.right = child.left;
-        node.parent = child;
-        child.parent = parent;
-        child.left = node;
+        node.right = right.left;
+        node.parent = right;
+        right.parent = parent;
+        right.left = node;
+        return root;
     }
 
     /**
      * 右旋转   右下
      */
-    private void rotateRight(RBTreeNode<T> node) {
-        RBTreeNode<T> child = node.left;
+    private RBTreeNode<T> rotateRight(RBTreeNode<T> root, RBTreeNode<T> node) {
+        RBTreeNode<T> left = node.left;
         RBTreeNode<T> parent = node.parent;
         if (parent == null) {
-            this.root = child;
+            root = left;
+            root.makeBlack();
         } else {
-            parent.left = child;
+            parent.left = left;
         }
-        node.left = child.right;
-        node.parent = child;
-        child.parent = parent;
-        child.right = node;
+        node.left = left.right;
+        node.parent = left;
+        left.parent = parent;
+        left.right = node;
+        return root;
     }
 
     /**
@@ -259,6 +327,10 @@ public class RBTree<T extends Comparable> {
             minNode = minNode.right;
         }
         return findNode;
+    }
+
+    private boolean isBlack(RBTreeNode<T> node) {
+        return node == null || node.isBlack();
     }
 
     public void preOrderByStack() {
